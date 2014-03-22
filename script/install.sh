495643eb8028edf384e0baf8a280e8e4f41b6868
D1plo1d <d1plo1d@d1plo1d.com>
2014-03-22 15:23:14 -0400

    Better install output.

--- script/install.sh
+++ script/install.sh
@@ -1,3 +1,4 @@
+#! /bin/sh
 DIR=$(cd $(dirname "$0"); pwd)
 path="/etc/tegh"
 cert="$path/cert"
@@ -8,27 +9,34 @@ sudo -v
 echo "Installing Tegh Daemon..."
 
 # Groups
-
+echo "Adding teghadmin Group..."
 sudo groupadd teghadmin | sudo dseditgroup -q -r "Tegh Admin" -o create teghadmin
+echo "Done"
 
 # Config File and Directories
 # ==============================================================================
 
+echo "Creating $path Directories..."
 sudo mkdir -p "$path/3d_printers/by_serial/"
 sudo chmod 655 $path
 sudo chmod 775 "$path/3d_printers/by_serial"
 sudo chown root:teghadmin "$path/3d_printers/by_serial"
 sudo cp "$DIR/../defaults/tegh.yml" "$path/tegh.yml"
+echo "Done"
 
 # PAM
 # ==============================================================================
 
 # Copying the sshd settings. Yes this is a massive hack.
 # If you have a better way that will work across os's please make the patch!
+echo "Setting up PAM Authentication..."
 sudo cp /etc/pam.d/sshd /etc/pam.d/tegh
+echo "Done"
 
 # SSL Cert
 # ==============================================================================
+
+echo "Generating SSL Certs..."
 
 # Generating the SSL Cert
 sudo openssl genrsa -des3 -passout pass:x -out "$cert.pass.key" 2048
@@ -44,3 +52,7 @@ sudo rm $cert.pass.key
 sudo rm $cert.key
 sudo rm $cert.csr
 sudo rm $cert.crt
+
+echo "Done"
+
+echo "Tegh Daemon Succesfully Installed."
